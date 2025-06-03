4981cf34d4deea4aa2d9e4007a7572324fe37b95
vijayharikumar02@gmail.com <Githu@887044>
2025-06-03 12:38:04 +0530

    bbb

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Link } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
 
 const router = createBrowserRouter([
@@ -7,16 +7,18 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
+    path: "/digital-marketing", 
+    element: <Home />,
+  },
+  {
     path: "*",
     element: (
-      <h1 className=" font-bold text-4xl text-center mt-40">
+      <div className="text-center mt-40 text-2xl font-bold">
         404 - Page Not Found
-        <Link to="/" className="text-blue-500 block mt-4 text-xl">
-          Go back to home
-        </Link>
-      </h1>
+      </div>
     ),
   },
 ]);
+
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/digital-marketing"
+  base: "/digital-marketing",
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    fs: {
+      allow: ['.'],
+    },
+  },
+  resolve: {
+    conditions: ['browser'],
+  },
 })
