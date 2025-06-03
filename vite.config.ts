b95a797f8ce7de1d0c00fb4e27d877d7f8b64ae5
vijayharikumar02@gmail.com <Githu@887044>
2025-06-03 13:17:26 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/digital-marketing",
+  base: "/",
   build: {
     outDir: 'dist',
   },
